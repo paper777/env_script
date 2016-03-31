0426571ff21cde665d4123c2f5d839e68d747b13
also <paper777@qq.com>
2016-03-31 16:53:46 +0800

    add git & screenrc config

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ yum groupinstall -y "Development Tools"
 cd $(dirname "$0")
 bash ./vimconf/install.sh
 bash ./zshconf/install.sh
+
+ls -fs $(pwd)/gitconfig ~/.gitconfig
+ls -fs $(pwd)/screenrc ~/.screenrc
